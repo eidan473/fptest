72d846856f7b0863212a5e650254f1fb9afabd15
eidan473 <eidanc1596@gmail.com>
2025-01-07 21:20:21 -0300

    ejercicios varios if else

--- text.py
+++ text.py
@@ -1,1 +1,73 @@
-print ("uwu?")
+from os import system
+system("cls")
+
+
+
+# nombre="diego"
+# edad=23
+
+# print ("mi nombre no es",nombre, " mi edad " ,edad)
+
+
+
+
+# print("ingrese un numero")
+# num1=int(input())
+# print ("ingrse otro numero")
+
+# num2=int(input())
+# print (num1+num2)
+
+
+
+
+
+
+# print("ingrese un numero")
+# num1=int(input())
+# if num1>=18:
+#     print("es mayor de edad")
+# else:
+#     print ("es menor de edad")
+
+
+
+
+
+
+# print("ingrese num")
+# num1=int (input())
+
+
+# print("ingrese num")
+# num2=int (input())
+
+
+# print ("ingrese num")
+# num3=int (input())
+
+
+# print((num1+num2+num3)/3)
+
+
+
+
+
+
+
+# print("ingrese edad")
+# num1=int(input())
+# if num1<12:
+#      print("su entrada es 500")
+# if num1>=13 and num1<=18:
+#      print ("su entrada es mil")
+# if num1>=19 and num1<=64:
+#     print ("su entrada es 2000")
+# if num1>65:
+#     print("su entrada es 700")
+    
+# print ("cuantas entradas desea")
+# num2=int(input())
+monto=200    
+print("pertence a la florida?")
+if 
