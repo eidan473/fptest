294117011f9067f98d8232464a2aba0596444161
eidan473 <eidanc1596@gmail.com>
2025-01-13 01:03:29 -0300

    uwu.estudio69 pro

--- estudiouwu.py
+++ estudiouwu.py
@@ -1,59 +1,95 @@
 from os import system
 system("cls")
-
-
-
-# con="uwu?"
+# total=0
 # while True:
-#     contraseña=(input("ingresa contraseña"))
-#     if contraseña==con:
-#         print("ta bien")
-#         break
+#     print("ingrese opcion")
+#     print("comprar=1")
+#     print("salir=2")
+#     opc=int(input())
+#     if opc==1:
+#         while True:
+#                 print("bienvenido al menu")
+#                 print("pan 1000=1")
+#                 print("bebia 2000=2")
+#                 print("cereal 1500=3")
+#                 print("pagar=4")
+#                 print("volver al menu principal=5")
+#                 op=int(input("ingrese lo que va a comprar"))
+#                 if op==1:
+#                      total=total+1000
+#                      print("usted quiere pan y su total es",total )
+#                 elif op==2:
+#                      total=total+2000
+#                      print("usted quiere bebia y su total es",total)
+#                 elif op==3:
+#                      total=total+1500
+#                      print("usted quiere cereal y su total es",total)
+#                 elif op==4:
+#                         while True:
+#                              print("con quiere pagar")
+#                              print("devito 20%=1")
+#                              print("credito=10%=2")
+#                              print("efectivo=3")
+#                              print("salir=4")
+#                              opci=int(input()) 
+#                              if opci==1:
+#                                 total=total*0.80
+#                                 print("usted pagara con devito y su total es ",total,"gracias por comprar")
+#                              elif opci==2:
+#                                   total=total*0.90
+#                                   print("pagara con creditoy su total es ",total,"gracias por comprar")
+#                              elif opci==3:
+#                                   print ("pagara con efectivo y su total es",total,"gracias por comprar")
+#                              elif opci==4:
+#                                   print("salir")
+#                                   break
+#                              else:
+#                                 print("nonono malo")
+#                 elif op==5:
+#                     print("vuelve al menu principal")
+#                     break
+#                 else:
+#                  print("nonono malo")
+#     if opc==2:
+#          print("salir")
+#          break
 #     else:
-#         print("ta mal")
+#         print("nonono malo")
 
-
-
-
-# edad=int(input("ingrese edad"))
-# ingreso=int(input("ingrese su sueldo"))
-# if edad>16 and ingreso>1000:
-#     print("puede tributar")
-# else:
-#     print ("no puede tributar")
-
-saldo=100
-
+import sys
 
 while True:
-    print("ver saldo=1")
-    print("sacar saldo=2")
-    print("depositar=3")
-    print("salir")                 
+    print("crear cuenta= 1")
+    print("inciar seccion=2")
+    print("salir")
     opc=int(input())
     if opc==1:
-        print("su saldo es",saldo)
-    elif opc==2:
-        sacar=int(input("cuanto va a sacar"))
-        if saldo<sacar:
-            print("error no tiene ese dinero")
-        elif saldo>=sacar:  
-            saldo=saldo-sacar
-            print ("usted tiene ",saldo)
-    elif opc==3:
-        depositar=int(input("cuanto deposita"))
-        saldo=saldo+depositar
-        print("saldo es",saldo)
-    elif opc==4:
-        print("salir")
-        break
-    else:
-        print("nonono malo")
-    
-    
+        while True:
+            print("crear nombre usuario")
+            nom=input()
+            print("crear contraseña")
+            contra=input()
+            break
+    if opc==2:
+        while True:
+             print("inciar seccion cual es su nombre de usuario")
+             nom1=input()
+             if nom1==nom:
+                print ("correcto")
+             elif nom1!=nom:
+                 print("siguiente")
+             con1=input("ingrese contra")
+             if con1==contra:
+                print("su contraseña es correcta")
+                print("bienvenido")
+                sys.exit()
+             elif con1!=contra:
+                print("contraseña o usuario incorrecta ")
+                
 
 
        
 
-    
+            
+
 
---
+++ uwu3.py
@@ -0,0 +1,55 @@
+
+# con="uwu?"
+# while True:
+#     contraseña=(input("ingresa contraseña"))
+#     if contraseña==con:
+#         print("ta bien")
+#         break
+#     else:
+#         print("ta mal")
+
+
+
+
+# edad=int(input("ingrese edad"))
+# ingreso=int(input("ingrese su sueldo"))
+# if edad>16 and ingreso>1000:
+#     print("puede tributar")
+# else:
+#     print ("no puede tributar")
+
+# saldo=100
+
+
+# while True:
+#     print("ver saldo=1")
+#     print("sacar saldo=2")
+#     print("depositar=3")
+#     print("salir")                 
+#     opc=int(input())
+#     if opc==1:
+#         print("su saldo es",saldo)
+#     elif opc==2:
+#         sacar=int(input("cuanto va a sacar"))
+#         if saldo<sacar:
+#             print("error no tiene ese dinero")
+#         elif saldo>=sacar:  
+#             saldo=saldo-sacar
+#             print ("usted tiene ",saldo)
+#     elif opc==3:
+#         depositar=int(input("cuanto deposita"))
+#         saldo=saldo+depositar
+#         print("saldo es",saldo)
+#     elif opc==4:
+#         print("salir")
+#         break
+    # else:
+
+
+    #     print("nonono malo")
+
+
+num =int (input("ingrese numero"))
+for i in range (num+1):
+    if i%2==0:
+        print("el numero no es par",i)
